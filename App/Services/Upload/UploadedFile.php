a44f179b0cd5a638e141183c172638d63aebfe6e
Mehrzad <mehrzadtajkarimi@gmail.com>
2021-07-25 17:41:28 +0430

    edit bog

--- App/Services/Upload/UploadedFile.php
+++ App/Services/Upload/UploadedFile.php
@@ -8,20 +8,12 @@ use App\Utilities\FlashMessage;
 class UploadedFile implements UploadContract
 {
     private $files;
-    private $file_names;
-    private $file_tmp_names;
-    private $path_in_storage;
+    private $paths_for_storage;
+    private $paths_for_database;
     const default_subfolder_format = "Ym";
 
-    public function __construct($file_params, $sub_folder = null)
+    public function __construct($file_params)
     {
-        if ($sub_folder == null) {
-            $sub_folder = date(self::default_subfolder_format);
-            $sub_folder_path = BASEPATH . 'Public/Storage/' . $sub_folder;
-            if (!file_exists($sub_folder_path)) {
-                mkdir($sub_folder_path);
-            }
-        }
         // array_keys($files_param)[0] output : name input file
         unset($file_params[array_keys($file_params)[0]]['error']);
         unset($file_params[array_keys($file_params)[0]]['size']);
@@ -34,59 +26,53 @@ class UploadedFile implements UploadContract
         }
 
         $count = count($this->files[array_keys($this->files)[0]]);
-
         for ($i = 0; $i < $count; $i++) {
-            $this->file_names['name'] = $this->files['name'];
-            $this->file_tmp_names['tmp_name'] = $this->files['tmp_name'];
-            $this->path_in_storage[] = $sub_folder . "/" . $this->basename($i) . '---' . $this->generateRandomStr() . $this->extension();
+            $paths_in_storage           = $this->generate_paths_in_storage($i);
+            $this->paths_for_database[] = base_url() . "Storage/$paths_in_storage";
+            $this->paths_for_storage[]  = BASEPATH . "Public/Storage/$paths_in_storage";
         }
-
-        echo '<pre>';
-        var_dump($this->files['name'][0]);
-        echo '</pre><br>';die;
     }
 
 
-    public function  basename($key)
+    public function  generate_paths_in_storage($key)
     {
-        return basename($this->name($key), $this->extension());
+        $name             = substr($this->files['name'][$key], 0, 64);
+        $name_explode     = explode('.', $name);
+        $type_file        = end($name_explode);
+        $basename         = basename($name, $type_file);
+        $basename_trim    = trim($basename);
+        $sub_folder_date  = date(self::default_subfolder_format);
+        $paths_in_storage = BASEPATH . 'Public/Storage/' . $sub_folder_date;
+        !file_exists($paths_in_storage) ? mkdir($paths_in_storage) : '';
+
+        return $sub_folder_date . '/' . $basename_trim . '---' . $this->generate_random_str() . '.' . $type_file;
     }
-    private function generateRandomStr()
+    private function generate_random_str()
     {
         return bin2hex(random_bytes(4));
-    }
-    public function extension()
-    {
-        $arr = explode('.', $this->name());
-        return '.' . end($arr);
-    }
-
-
-    public function name($key)
-    {
-        return substr($this->file_names[$key], 0, 128);
-    }
-
-    private function upload($path)
-    {
-        return move_uploaded_file($this->file['tmp_name'], $path);
     }
 
     public function destroy()
     {
-        $path        =  BASEPATH . 'Public/Storage/'  . $this->path_in_storage;
-        if (!file_exists($path)) {
+        if (!file_exists($this->paths_for_storage)) {
             return;
         }
-        return unlink($path);
+        return unlink($this->paths_for_storage);
     }
+
+    private function upload()
+    {
+        echo '<pre>';
+        var_dump($this->paths_for_storage);
+        echo '</pre><br>';die;
+        foreach ($this->files['tmp_name'] as $key => $path) {
+            return move_uploaded_file($path, $this->paths_for_storage[$key]);
+        }
+    }
+
 
     public function save()
     {
-        $path        =  BASEPATH . 'Public/Storage/' . $this->path_in_storage;
-        if ($this->upload($path)) {
-            return storage_url($this->path_in_storage);
-        }
-        return false;
+        return $this->upload() ? $this->paths_for_database : false;
     }
 }
