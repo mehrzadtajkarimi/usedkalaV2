de73619958095ca5f56ba9ea9faf1624c2c02d3b
Mehrzad <mehrzadtajkarimi@gmail.com>
2021-12-08 13:35:08 +0330

    add EN for show access admin for panel

--- App/Views/Backend/access/script.php
+++ App/Views/Backend/access/script.php
@@ -36,23 +36,33 @@
                     $(data.permission).each(function(key, value) {
                         console.log(data.permission);
                         permission.append(`
-                        <div>
+                        <div class='row'>
+                        <div class='col'>
                         <a href='<?= base_url() ?>admin/access/delete_access/permission/` + value[2] + `' onclick="return confirm('آیا برای حذف اطلاعات اطمینان دارید');">
                         <i class='ml-3 fa fa-times-circle text-danger fa-lg'></i>
-                        </a>    
+                        </a>
                         ` + value[1] + `
-                        </div>  
+                        </div>
+                        <div class='col'>
+                        <span class='text-muted '>` + value[0] + `</span>
+                        </div>
+                        </div>
                         `);
                     });
                     role.empty();
                     $(data.role).each(function(key, value) {
                         console.log(data.role);
                         role.append(`
-                        <div>
+                        <div class='row'>
+                        <div class='col'>
                         <a href='<?= base_url() ?>admin/access/delete_access/role/` + value[2] + `' onclick="return confirm('آیا برای حذف اطلاعات اطمینان دارید');">
                         <i class='ml-3 fa fa-times-circle text-danger fa-lg'></i>
                         </a>
-                        ` + value[1] + `
+                        <span>` + value[1] + `</span>
+                        </div>
+                        <div class='col'>
+                        <span class='text-muted '>` + value[0] + `</span>
+                        </div>
                         </div>
                         `);
                     });
